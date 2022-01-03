1360e545b34c006ec7632a62b37e626457874bf9
chakri <86776511+chakrinya009@users.noreply.github.com>
2022-01-03 23:58:43 +0530

    Update insertion.cpp

--- arrays/insertion.cpp
+++ arrays/insertion.cpp
@@ -17,7 +17,7 @@ int main()
  cout<<endl;
  for(i=0;i<n;i++)
  {
-     cin>>a[i];
+     cin>>a[i];                                 //taking elements from the user
  }
  cout<<"inserting element into the array:"<<endl;
  for(i=n-1;i>=0;i--)
