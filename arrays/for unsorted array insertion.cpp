8c5eb736d6d46a98b8729cf46e73b121944e12e5
chakrinya <chakrinyabalabadra@gmail.com>
2022-01-05 18:38:08 +0530

    inserting the unsorted

---
+++ arrays/for unsorted array insertion.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,num,pos,size;
+    cout<<"enter the size of the array:"<<endl;
+    cin>>size;
+    int a[size];
+    cout<<"enter the number to be inserted:"<<endl;
+    cin>>num;
+    cout<<"enter the position :"<<endl;
+    cin>>pos;
+
+    cout<<"enter the elements:"<<endl;
+    for(i=0;i<size;i++)
+    {
+        int x;
+        cin>>x;
+        a[i]=x;
+    }
+
+    a[size]=a[pos-1];
+    a[pos-1]=num;
+    size++;
+
+    cout<<"display the elements:"<<endl;
+    for(i=0;i<size;i++)
+      {
+          cout<<a[i]<<" ";
+      }
+
+      return 0;
+
+}
+
+
